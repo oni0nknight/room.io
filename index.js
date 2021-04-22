6db94d385f59e8099876725ef49e8a7b678313b8
Théophile Gindre <theophile.gindre@gmail.com>
2021-04-22 02:16:58 +0200

    major update for Logger, RoomPusher, and Errors

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-import { Server } from './lib/Server'
-import { Conditions } from './lib/Conditions'
+const Server = require('./lib/Server')
 
-export function createServer()
+/**
+ * @typedef {Object} SubscribeDescriptor
+ * @property {string} name The subscribe name. The Game class must have a method with that name
+ * @property {[Function]} inputValidator A validation function run on the user input
+ */
+
+
+/**
+ * Create and return a Server instance
+ * @param {Object} config config object for the server
+ *      - gameClass: A Game class
+ *      - {Array.<SubscribeDescriptor>} list of subscribes
+ * @returns {Server} an instance of Server
+ */
+function createServer(config)
 {
     /**
      * Create the Server instance
      * @type {Server}
      */
-    const server = new Server()
+    const server = new Server(config)
 
     return server
 }
 
-module.exports = { createServer, Conditions }
+module.exports = {
+    createServer
+}
